2e59346e473c7dee8832e8d29e1a44b51997d201
Tomek <tomusiek06@gmail.com>
2017-11-05 18:40:11 +0100

    database fix 2

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,11 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
+
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+
 
 module Projekt
   class Application < Rails::Application
@@ -15,3 +17,4 @@ module Projekt
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
