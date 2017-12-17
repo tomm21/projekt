bbffce3d8e1b327c6abdd795c99b543876ca246a
Tomek <tomusiek06@gmail.com>
2017-12-18 00:56:04 +0100

    naprawa bledu2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :reviews
-  ratyrate_rater
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :user_name, :uniqueness => { :case_sensitive => false }
