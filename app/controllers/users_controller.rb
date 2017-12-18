92c85ceeedeb1134516aba75e958c67a18190d35
Tomek <tomusiek06@gmail.com>
2017-12-18 16:43:12 +0100

    dodanie top5 filmow na stronie głownej oraz ostatnio dodanych filmow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 before_action :find_user
-
+before_action :find_reviews, only: [:show]
 def index
 end
 
@@ -8,7 +8,10 @@ def show
 end
 
 private
-		def find_user
-			@user = User.find(params[:id])
-		end
+	def find_user
+		@user = User.find(params[:id])
+	end
+	def find_reviews
+	    @reviews = @user.reviews
+    end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
+before_action :find_latest_movies, only: [:index]
+before_action :find_highest_rated, only: [:index]
+
   def index
   end
+  private
+  	def find_latest_movies 
+  		@movies = Movie.order('created_at DESC').last(5)
+  	end
+
+  	def find_highest_rated
+  		@moviesRated = Movie.order('rating DESC').first(5)
+  	end
 end
